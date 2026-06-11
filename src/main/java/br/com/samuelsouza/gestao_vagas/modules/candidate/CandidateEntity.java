d3e53fcd2e5f86585f1e4759537db027c91856b2
samuel <smksouza.dev@gmail.com>
2026-06-11 20:47:30 -0300

    Inserindo validacao

--- src/main/java/br/com/samuelsouza/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/samuelsouza/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,25 @@
 package br.com.samuelsouza.gestao_vagas.modules.candidate;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
-
 @Data
 public class CandidateEntity {
 
     private UUID id;
     private  String name;
+
+    @Pattern(regexp = "^(?!\\\\s*$).+", message = "O Campo [username] nao deve conter espaco ")
     private  String username;
+
+    @Email( message = "O Campo [e-mail] deve conter um e-mail valido")
     private  String email;
+
+    @Length(min = 5, max = 12)
     private  String password;
     private  String description;
     private  String curriculum;
--- src/main/java/br/com/samuelsouza/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/samuelsouza/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,6 +2,7 @@ package br.com.samuelsouza.gestao_vagas.modules.candidate.controllers;
 
 
 import br.com.samuelsouza.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
 
