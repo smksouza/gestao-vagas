79f6837e4942ee0bfa42d7607fbec0cdee77e84a
samuel <smksouza.dev@gmail.com>
2026-06-14 21:36:43 -0300

    feat: adicionar CompanyEntity e refatorar criação de candidato para use case

---
+++ src/main/java/br/com/samuelsouza/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,22 @@
+package br.com.samuelsouza.gestao_vagas.modules.candidate.useCases;
+
+import br.com.samuelsouza.gestao_vagas.exceptions.UserFoundException;
+import br.com.samuelsouza.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.samuelsouza.gestao_vagas.modules.candidate.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CreateCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+  public CandidateEntity execute(CandidateEntity candidateEntity) {
+      this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent((user) -> {
+          throw new UserFoundException();
+      });
+      return this.candidateRepository.save((candidateEntity));
+    }
+}
--- src/main/java/br/com/samuelsouza/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/samuelsouza/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -1,15 +1,12 @@
-package br.com.samuelsouza.gestao_vagas.modules.candidate;
+package br.com.samuelsouza.gestao_vagas.modules.company.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
-
 import lombok.Data;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
@@ -17,25 +14,25 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@Entity(name = "candidate")
-public class CandidateEntity {
+@Entity(name = "company")
+public class CompanyEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    private  String name;
-
     @Pattern(regexp = "^\\S+$", message = "O Campo [username] nao deve conter espaco ")
-    private  String username;
+    private String username;
 
     @Email( message = "O Campo [e-mail] deve conter um e-mail valido")
-    private  String email;
+    private String email;
 
     @Length(min = 5, max = 12, message = "A senha deve conter entre 5 a 12 caracteres")
-    private  String password;
-    private  String description;
-    private  String curriculum;
+    private String password;
+    
+    private String name;
+    private String website;
+    private String description;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
