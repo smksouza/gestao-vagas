b354c2bdacfb8a26b7ddbaad7cbef22c986df3dd
samuel <smksouza.dev@gmail.com>
2026-06-18 21:50:04 -0300

    feat: finalizando use case de autenticação da empresa com JWT

--- src/main/java/br/com/samuelsouza/gestao_vagas/modules/candidate/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/samuelsouza/gestao_vagas/modules/candidate/useCases/AuthCompanyUseCase.java
@@ -2,7 +2,10 @@ package br.com.samuelsouza.gestao_vagas.modules.candidate.useCases;
 
 import br.com.samuelsouza.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.samuelsouza.gestao_vagas.modules.company.repositories.CompanyRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,15 +14,19 @@ import javax.naming.AuthenticationException;
 
 @Service
 public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
     @Autowired
     private CompanyRepository companyRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-    public void execute(AuthCompanyDTO authCompanyDTO) {
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException  {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
                 () -> {
-                    throw new UsernameNotFoundException("Company not found");
+                    throw new UsernameNotFoundException("Username/password incorrect");
                 });
 
             var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
@@ -27,5 +34,10 @@ public class AuthCompanyUseCase {
             if (!passwordMatches) {
                 throw new AuthenticationException();
             }
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+      var token = JWT.create().withIssuer("nomeempresa")
+        .withSubject(company.getId().toString())
+        .sign(algorithm);
+        return token;
     }
 }
